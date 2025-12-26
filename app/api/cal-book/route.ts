44464506709087a1b4d7d2caabdee2d13173b7b2
Latasha1507 <latasha15ukey@gmail.com>
2025-12-26 22:19:53 +0530

    Fix

--- app/api/cal-book/route.ts
+++ app/api/cal-book/route.ts
@@ -1,7 +1,6 @@
 import { NextResponse } from 'next/server'
 import { createClient } from '@/lib/supabase/server'
-// @ts-ignore
-import Resend from 'resend'
+import { Resend } from 'resend'
 
 const resend = new Resend(process.env.RESEND_API_KEY!)
 
